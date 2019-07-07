030c0871c684cdd901d711174c03b6e4f4d8deba
MS <mixa.ru.ru@gmail.com>
2019-07-07 20:53:12 +0300

    type + README.md

--- README.md
+++ README.md
@@ -4,21 +4,9 @@ docker run -it --rm -u $(id -u ${USER}):$(id -g ${USER}) -v ${PWD}:/app node:11 
 ```
 inside docker container run
 ```bash
-cd app && yarn init -y
+cd app && yarn install
 ```
-## 2 install brain.js
-inside docker container run 
-```bash
-yarn add brain.js
-```
-
-## 3 install webpack as DEV dependency
-run also inside container into /app folder
-```bash
-yarn add webpack webpack-cli -D
-```
-
-## 4 to build a project
+## 2 to build a project
 run ```yarn dev``` or ```yarn build``` for minified version inside docker container in /app folder 
 
 ## demo link
--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,8 @@ function DCanvas(el) {
 
     let is_mouse_down = false;
 
-    canv.width = 500;
-    canv.height = 500;
+    el.width = 500;
+    el.height = 500;
 
     this.drawLine = function (x1, y1, x2, y2, color = 'green') {
         ctx.beginPath();
@@ -33,12 +33,12 @@ function DCanvas(el) {
     };
 
     this.clear = function () {
-        ctx.clearRect(0,0, canv.width, canv.height);
+        ctx.clearRect(0,0, el.width, el.height);
     };
 
     this.drawGrid = function() {
-        const w = canv.width;
-        const h = canv.height;
+        const w = el.width;
+        const h = el.height;
         const p = w / pixel;
 
         const xStep = w / p;
@@ -53,6 +53,50 @@ function DCanvas(el) {
         {
             this.drawLine(0, y, w, y);
         }
+    };
+
+    this.calculate = function(draw = false) {
+        const w = el.width;
+        const h = el.height;
+        const p = w / pixel;
+
+        const xStep = w / p;
+        const yStep = h / p;
+
+        const vector = [];
+        let __draw = [];
+
+        for (let x = 0; x < w; x += xStep) {
+            for(let y = 0; y < h; y += yStep) {
+                const data = ctx.getImageData(x, y, xStep, yStep);
+
+                let nonEmptyPixelsCount = 0;
+                for( i = 0; i < data.data.length; i += 10 ) {
+                    const isEmpty = data.data[i] === 0;
+
+                    if( !isEmpty ) {
+                        nonEmptyPixelsCount += 1;
+                    }
+                }
+
+                if( nonEmptyPixelsCount > 1 && draw ) {
+                    __draw.push([x, y, xStep, yStep]);
+                }
+
+                vector.push(nonEmptyPixelsCount > 1 ? 1 : 0);
+            }
+        }
+
+        if(draw) {
+            this.clear();
+            this.drawGrid();
+
+            for(_d in __draw) {
+                this.drawCell( __draw[_d][0], __draw[_d][1], __draw[_d][2], __draw[_d][3] );
+            }
+        }
+
+        return vector;
     };
     
     el.addEventListener('mousedown', function () {
